caa1346d763d85b37b14d31a18ba784034a36fa6
Brayan Jenzy La Cruz Carreño <blacruzc@emeal.nttdata.com>
2022-04-11 09:36:37 -0500

    servicio de activos

---
+++ actives-service/src/main/java/com/nttdata/actives/ActivesApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.actives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivesApplication.class, args);
+	}
+
+}
---
+++ actives-service/src/test/java/com/nttdata/actives/ActivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.actives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
